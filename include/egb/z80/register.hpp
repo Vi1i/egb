5c9a55cedfd88c44c4f59f95007b42d91a640da4
Vi1i <vi1i.petal@gmail.com>
2019-11-04 10:56:43 -0500

    Updated the removal of the register source to headers. Endianess is handled int he MMU.

--- include/egb/z80/register.hpp
+++ include/egb/z80/register.hpp
@@ -6,10 +6,9 @@ namespace egb::z80 {
   union Register {
     std::uint16_t w;
 
-    //This is set this way for little-endianness
     struct {
-      std::uint8_t l;
       std::uint8_t h;
+      std::uint8_t l;
     };
   };
 }
